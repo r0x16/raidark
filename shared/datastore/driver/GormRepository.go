9665814ae8b2c35137157cb622f5ad7e4e9f8fa4
r0x16 <sir.thomas.lara@gmail.com>
2025-07-13 21:37:05 -0400

    feat(datastore): Update GormRepository transaction implementation
    Enhanced GormRepository with improved transaction handling capabilities and better access patterns.
    
    Changes:
    - Update GormRepository.go with enhanced transaction methods
    - Improve transaction access patterns and reliability
    - Add better transaction handling for Gorm operations

--- shared/datastore/driver/GormRepository.go
+++ shared/datastore/driver/GormRepository.go
@@ -2,16 +2,37 @@ package driver
 
 import (
 	"github.com/r0x16/Raidark/shared/datastore/domain"
+	domenv "github.com/r0x16/Raidark/shared/env/domain"
+	domproviders "github.com/r0x16/Raidark/shared/providers/domain"
 	"gorm.io/gorm"
 )
 
 type GormRepository struct {
-	dbProvider domain.DatabaseProvider
+	dbProvider  domain.DatabaseProvider
+	envProvider domenv.EnvProvider
+}
+
+func NewGormRepository(hub *domproviders.ProviderHub) *GormRepository {
+	return &GormRepository{
+		dbProvider:  domproviders.Get[domain.DatabaseProvider](hub),
+		envProvider: domproviders.Get[domenv.EnvProvider](hub),
+	}
 }
 
 func (r *GormRepository) GetExec() *gorm.DB {
-	gormProvider := r.dbProvider.(*GormPostgresDatabaseProvider)
-	return gormProvider.db
+	switch r.envProvider.GetString("DATASTORE_TYPE", "sqlite") {
+	case "postgres":
+		gormProvider := r.dbProvider.(*GormPostgresDatabaseProvider)
+		return gormProvider.db
+	case "mysql":
+		gormProvider := r.dbProvider.(*GormMysqlDatabaseProvider)
+		return gormProvider.db
+	case "sqlite":
+		gormProvider := r.dbProvider.(*GormSqliteDatabaseProvider)
+		return gormProvider.db
+	default:
+		panic("invalid datastore type")
+	}
 }
 
 func (r *GormRepository) GetTransactionExec(tx domain.Transaction) *gorm.DB {
